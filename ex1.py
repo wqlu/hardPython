acb0db091dd26d76cb1496d4982a33dfd41816f1
Lu511 <lu511@outlook.com>
2017-09-12 15:07:44 +0800

    2017.9.12

---
+++ ex1.py
@@ -0,0 +1,5 @@
+print("hello world!")
+print("hello again")
+print("i like typing this")
+print("this is fun")
+print("yay ! print")
---
+++ ex2.py
@@ -0,0 +1,4 @@
+# A comment, this is so you can read your program later.
+print("i could have code like this") # and the comment after is ignored
+# print("this is fun")
+print("this will run")
